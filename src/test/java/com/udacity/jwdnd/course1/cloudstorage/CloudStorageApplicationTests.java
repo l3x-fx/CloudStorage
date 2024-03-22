f3adfce62bf25b0c8ff0581fed92bfb821a9bc7d
l3x-fx <kekskiller@email.com>
2024-03-22 14:23:38 +0100

    tests complete

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -2,10 +2,7 @@ package com.udacity.jwdnd.course1.cloudstorage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -14,6 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
 import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudStorageApplicationTests {
 
@@ -21,7 +21,7 @@ class CloudStorageApplicationTests {
 	private int port;
 
 	private WebDriver driver;
-
+	private Helper helper;
 	@BeforeAll
 	static void beforeAll() {
 		WebDriverManager.chromedriver().setup();
@@ -30,6 +30,7 @@ class CloudStorageApplicationTests {
 	@BeforeEach
 	public void beforeEach() {
 		this.driver = new ChromeDriver();
+		this.helper = new Helper(this.driver);
 	}
 
 	@AfterEach
@@ -45,89 +46,42 @@ class CloudStorageApplicationTests {
 		Assertions.assertEquals("Login", driver.getTitle());
 	}
 
-	private void doMockSignUp(String firstName, String lastName, String userName, String password) throws InterruptedException {
-		// Create a dummy account for logging in later.
-
+	private void doMockSignUp(String firstName, String lastName, String userName, String password) {
 		// Visit the sign-up page.
 		WebDriverWait webDriverWait = new WebDriverWait(driver, 2);
 		driver.get("http://localhost:" + this.port + "/signup");
 		webDriverWait.until(ExpectedConditions.titleContains("Sign Up"));
 		
 		// Fill out credentials
-		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputFirstName")));
-		WebElement inputFirstName = driver.findElement(By.id("inputFirstName"));
-		inputFirstName.click();
-		inputFirstName.sendKeys(firstName);
-
-		Thread.sleep(600);
-
-		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputLastName")));
-		WebElement inputLastName = driver.findElement(By.id("inputLastName"));
-		inputLastName.click();
-		inputLastName.sendKeys(lastName);
-
-		Thread.sleep(600);
-
-		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputUsername")));
-		WebElement inputUsername = driver.findElement(By.id("inputUsername"));
-		inputUsername.click();
-		inputUsername.sendKeys(userName);
-
-		Thread.sleep(600);
-
-		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputPassword")));
-		WebElement inputPassword = driver.findElement(By.id("inputPassword"));
-		inputPassword.click();
-		inputPassword.sendKeys(password);
-
-		Thread.sleep(600);
-
-		// Attempt to sign up.
-		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("buttonSignUp")));
-		WebElement buttonSignUp = driver.findElement(By.id("buttonSignUp"));
-		buttonSignUp.click();
-
-		/* Check that the sign up was successful. 
-		// You may have to modify the element "success-msg" and the sign-up 
-		// success message below depening on the rest of your code.
-		*/
-
-		Thread.sleep(600);
+		helper.enterTextById("inputFirstName", firstName);
+		helper.enterTextById("inputLastName", lastName);
+		helper.enterTextById("inputUsername", userName);
+		helper.enterTextById("inputPassword", password);
+		helper.clickById("buttonSignUp");
 
 		Assertions.assertTrue(driver.findElement(By.id("successMsg")).getText().contains("Registered successfully! Log in to continue."));
-
-		Thread.sleep(600);
 	}
 
-	private void doLogIn(String userName, String password) throws InterruptedException {
+	private void doLogIn(String userName, String password) {
 		// Log in to our dummy account.
 		driver.get("http://localhost:" + this.port + "/login");
 		WebDriverWait webDriverWait = new WebDriverWait(driver, 2);
 
-		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputUsername")));
-		WebElement loginUserName = driver.findElement(By.id("inputUsername"));
-		loginUserName.click();
-		loginUserName.sendKeys(userName);
+		helper.enterTextById("inputUsername", userName);
+		helper.enterTextById("inputPassword", password);
+		helper.clickById("submitButton");
 
-		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputPassword")));
-		WebElement loginPassword = driver.findElement(By.id("inputPassword"));
-		loginPassword.click();
-		loginPassword.sendKeys(password);
-
-		webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("submitButton")));
-		WebElement loginButton = driver.findElement(By.id("submitButton"));
-		loginButton.click();
-		Thread.sleep(600);
 		webDriverWait.until(ExpectedConditions.titleContains("Home"));
-		Thread.sleep(600);
+	}
+
+	private void verifyHomeAccess(){
+		driver.get("http://localhost:" + this.port + "/home");
+		Assertions.assertEquals("http://localhost:" + this.port + "/home", driver.getCurrentUrl());
 	}
 
 	@Test
 	public void testRedirection() throws InterruptedException {
-		// Create a test account
 		doMockSignUp("Redirection","Test","RT","123");
-		
-		// Check if we have been redirected to the log in page.
 		Assertions.assertEquals("http://localhost:" + this.port + "/login", driver.getCurrentUrl());
 	}
 
@@ -142,6 +96,153 @@ class CloudStorageApplicationTests {
 		Thread.sleep(200);
 		Assertions.assertFalse(driver.getPageSource().contains("Whitelabel Error Page"));
 	}
+
+
+//	SIGNUP AND LOVING FLOW
+	@Test
+	public void testUnauthorizedHomeAccess()  {
+//		home page is not accessible without logging in.
+		driver.get("http://localhost:" + this.port + "/home");
+		Assertions.assertNotEquals("http://localhost:" + this.port + "/home", driver.getCurrentUrl());
+
+	}
+	@Test
+	public void testHomeAccessAfterLogout() throws InterruptedException {
+		doMockSignUp("Home","Logout","HL","one");
+		doLogIn("HL", "one");
+
+		verifyHomeAccess();
+
+		helper.clickById("logoutButton");
+
+		driver.get("http://localhost:" + this.port + "/home");
+		Thread.sleep(600);
+		Assertions.assertNotEquals("http://localhost:" + this.port + "/home", driver.getCurrentUrl());
+	}
+
+// NOTES : ADD - EDIT - DELETE
+	private void createNote()  {
+		helper.clickById("nav-notes-tab");
+		helper.clickById("add-new-note");
+		helper.enterTextById("note-title", "NOTE TITLE");
+		helper.enterTextById("note-description", "NOTE TEXT");
+		helper.clickById("note-submit");
+		driver.get("http://localhost:" + this.port + "/home");
+		helper.clickById("nav-notes-tab");
+	}
+	@Test
+	public void testAddNote() throws InterruptedException {
+		WebDriverWait wait = new WebDriverWait(driver, 2);
+		doMockSignUp("Add","Note","AN","123");
+		doLogIn("AN", "123");
+		createNote();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//tr[starts-with(@id, 'note')]")));
+		WebElement note = driver.findElement(By.xpath("//tr[starts-with(@id, 'note')]"));
+		Assertions.assertTrue(note.isDisplayed());
+	}
+
+	@Test
+	public void testEditNote() throws InterruptedException {
+		WebDriverWait wait = new WebDriverWait(driver, 2);
+
+		doMockSignUp("Edit","Note","EN","123");
+		doLogIn("EN", "123");
+
+		createNote();
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//tr[starts-with(@id, 'note')]")));
+		helper.clickByPath("button","edit-note");
+		helper.enterTextById("note-description", " EDITED");
+		helper.clickById("note-submit");
+		driver.get("http://localhost:" + this.port + "/home");
+		helper.clickById("nav-notes-tab");
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//tr[starts-with(@id, 'note')]")));
+		WebElement note = driver.findElement(By.xpath("//td[starts-with(@id, 'text-note')]"));
+		Assertions.assertTrue(note.getText().contains("EDITED"));
+	}
+
+	@Test
+	public void testDeleteNote() throws InterruptedException {
+		WebDriverWait wait = new WebDriverWait(driver, 2);
+
+		doMockSignUp("Delete","Note","DN","123");
+		doLogIn("DN", "123");
+
+		createNote();
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//tr[starts-with(@id, 'note')]")));
+
+		helper.clickByPath("a", "delete-note");
+
+		driver.get("http://localhost:" + this.port + "/home");
+
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//tr[starts-with(@id, 'note')]")));
+		assertThrows(NoSuchElementException.class, () -> driver.findElement(By.xpath("//tr[starts-with(@id, 'note')]")));
+		}
+
+// CREDENTIALS _ ADD - EDIT - DELETE
+	private void createCredential()  {
+	helper.clickById("nav-credentials-tab");
+	helper.clickById("add-new-credential");
+	helper.enterTextById("credential-url", "www.test.me");
+	helper.enterTextById("credential-username", "USER");
+	helper.enterTextById("credential-password", "PASS");
+	helper.clickById("credential-submit");
+	driver.get("http://localhost:" + this.port + "/home");
+	helper.clickById("nav-credentials-tab");
+}
+	@Test
+	public void testAddCredential() throws InterruptedException {
+		WebDriverWait wait = new WebDriverWait(driver, 2);
+		doMockSignUp("Add","Cred","AC","123");
+		doLogIn("AC", "123");
+		createCredential();
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//tr[starts-with(@id, 'cred')]")));
+		WebElement cred = driver.findElement(By.xpath("//tr[starts-with(@id, 'cred')]"));
+		Assertions.assertTrue(cred.isDisplayed());
+	}
+
+	@Test
+	public void testEditCredential() throws InterruptedException {
+		WebDriverWait wait = new WebDriverWait(driver, 2);
+
+		doMockSignUp("Edit","Cred","EC","123");
+		doLogIn("EC", "123");
+
+		createCredential();
+		helper.clickByPath("button", "edit-cred");
+
+		helper.enterTextById("credential-username", " EDITED");
+		helper.clickById("credential-submit");
+		driver.get("http://localhost:" + this.port + "/home");
+		helper.clickById("nav-credentials-tab");
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//tr[starts-with(@id, 'cred')]")));
+		WebElement note = driver.findElement(By.xpath("//td[starts-with(@id, 'user-cred')]"));
+		Assertions.assertTrue(note.getText().contains("EDITED"));
+	}
+	@Test
+	public void testDeleteCredential() throws InterruptedException {
+		WebDriverWait wait = new WebDriverWait(driver, 2);
+
+		doMockSignUp("Delete","Credential","DC","123");
+		doLogIn("DC", "123");
+
+		createCredential();
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[starts-with(@id, 'delete-cred')]")));
+		WebElement button = driver.findElement(By.xpath("//a[starts-with(@id, 'delete-cred')]"));
+		button.click();
+
+		driver.get("http://localhost:" + this.port + "/home");
+
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//tr[starts-with(@id, 'cred')]")));
+		assertThrows(NoSuchElementException.class, () -> driver.findElement(By.xpath("//tr[starts-with(@id, 'cred')]")));
+	}
+
+	//FILE UPLOAD
 
 	@Test
 	public void testLargeUpload() throws InterruptedException {
@@ -167,14 +268,4 @@ class CloudStorageApplicationTests {
 		Assertions.assertFalse(driver.getPageSource().contains("HTTP Status 403 – Forbidden"));
 
 	}
-//	SIGNUP AND LOVING FLOW
-	@Test
-	public void testUnauthorizedHomeAccess() throws InterruptedException {
-
-	}
-	@Test
-	public void testHomeAccessAfterLogout() throws InterruptedException {
-
-	}
-
 }
---
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/Helper.java
@@ -0,0 +1,36 @@
+package com.udacity.jwdnd.course1.cloudstorage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Helper {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public Helper(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 2);
+    }
+    public void clickById(String id) {
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(id)));
+        WebElement button = driver.findElement(By.id(id));
+        button.click();
+    }
+    public void clickByPath(String element, String idFragment) {
+        String path = String.format("//%s[starts-with(@id, '%s')]", element, idFragment);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(path)));
+        WebElement button = driver.findElement(By.xpath(path));
+        button.click();
+    }
+    public void enterTextById(String id, String text) {
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(id)));
+        WebElement element = driver.findElement(By.id(id));
+        element.click();
+        element.sendKeys(text);
+    }
+}
